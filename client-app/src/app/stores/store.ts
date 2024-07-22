5c09a93a30bf0dbf0c4e418fa5d0d94eb4c96e1f
Haseeb Kalwar <haseebkalwar.hgc1@gmail.com>
2024-07-22 15:30:24 -0400

    Profile Image API consume client side

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,21 +1,25 @@
 import ActivityStore from "./activityStore";
 import {createContext, useContext} from "react";
 import CommonStore from "./commonStore";
-import UserStore from "../models/userStore";
 import ModalStore from "./modalStore";
+import ProfileStore from "./ProfileStore";
+import UserStore from "./userStore";
+
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
     userStore: UserStore;
     modalStore: ModalStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
     userStore: new UserStore(),
-    modalStore: new ModalStore()
+    modalStore: new ModalStore(),
+    profileStore: new ProfileStore()
 }
 
 export const StoreContext = createContext(store);
