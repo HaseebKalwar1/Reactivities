b6ef8d98eba98122c3c3797f6ecbcd1de6ce46b0
Haseeb Kalwar <haseebkalwar.hgc1@gmail.com>
2024-07-29 13:28:15 -0400

    Paging, Filtering and Sorting

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,18 +1,24 @@
-import { Grid } from "semantic-ui-react";
+import { observer } from 'mobx-react-lite';
+import { useEffect, useState } from 'react';
+import { Grid, Loader } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
-import { useStore } from "../../../app/stores/store";
-import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
-
+import ActivityListItemPlaceholder from './ActivityListItemPlaceHolder';
+import { PagingParams } from '../../../app/models/Pagination';
+import InfiniteScroll from 'react-infinite-scroller';
 
 export default observer(function ActivityDashboard() {
-    const {activityStore} = useStore();
-    const {loadActivities, activityRegistry} = activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, setPagingParams, pagination } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
 
-    useEffect(() => {
-      if (activityRegistry.size <= 1) loadActivities();
-    },[loadActivities])
+  function handleGetNext() {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1));
+    loadActivities().then(() => setLoadingNext(false));
+}
+
     /* without  Axios Method 
     useEffect(() => {
       fetch('http://localhost:5002/api/activities')
@@ -21,15 +27,34 @@ export default observer(function ActivityDashboard() {
         .catch(error => console.error(error));
     }, []);
   */
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
+    useEffect(() => {
+      loadActivities();
+  }, [loadActivities])
   
     return (
         <Grid>
             <Grid.Column width='10'>
-            <ActivityList />
+                {activityStore.loadingInitial && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                        <InfiniteScroll
+                            pageStart={0}
+                            loadMore={handleGetNext}
+                            hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                            initialLoad={false}
+                        >
+                            <ActivityList />
+                        </InfiniteScroll>
+                    )}
             </Grid.Column>
             <Grid.Column width='6'>
-              <h2>Activity Dashboard</h2>
+                <ActivityFilters />
+            </Grid.Column>
+            <Grid.Column width='10'>
+                <Loader active={loadingNext} />
             </Grid.Column>
 
         </Grid>
